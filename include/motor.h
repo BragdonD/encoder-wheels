53ef9dce341c1f5e3c0e7321492ed65bfecea5bb
BragdonD <73294632+BragdonD@users.noreply.github.com>
2022-02-15 23:26:57 +0100

    add motor.h

---
+++ include/motor.h
@@ -0,0 +1,28 @@
+#if !defined(MOTOR_H)
+#define MOTOR_H
+
+#include <Arduino.h>
+
+///define direction
+#define FORWARD 0x1
+#define BACKWARD 0x0
+
+///Define motors pin
+#define MOTOR1_DIR_PIN 0U
+#define MOTOR2_DIR_PIN 2U
+#define MOTOR1_SPD_PIN 5U
+#define MOTOR2_SPD_PIN 4U
+
+typedef struct motor
+{
+    uint8_t dir_pin;    ///direction pin (D3, D4)
+    uint8_t spd_pin;    ///speed pin (D1, D2)
+    uint8_t direction;  ///direction of the motor (FORWARD, BACKWARD)
+    float speed;        ///rotation speed
+}t_motor, motor;
+
+void MooveForward (motor *x, float speed);
+void MooveBackward (motor *x, float speed);
+void Moove (motor x);
+
+#endif // MOTOR_H
